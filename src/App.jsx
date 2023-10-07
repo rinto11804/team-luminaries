93c7ed0c51b12d4d4f6e2c80a953fa76626d1146
AlfrinP <alfrinpoulose@gmail.com>
2023-10-07 17:46:44 +0530

    added tour section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Testimonials from "./components/Testimonials";
+import Tours from "./components/Tours";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Home />
       <Testimonials />
+      <Tours />
     </>
   );
 }
