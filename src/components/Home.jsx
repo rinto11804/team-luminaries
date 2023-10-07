15febc5ab23cc439a426642b172ce759a24dc616
AlfrinP <alfrinpoulose@gmail.com>
2023-10-07 14:05:46 +0530

    added pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='font-bold'>Alfrin Poulose</h1>
+      <nav>
+        <ul>
+          <li>Tours</li>
+          <li>Services</li>
+          <li>Testimonials</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
